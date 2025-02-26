355368e6d4e8fb10c45fa139fb6ce37d6ab7596b
cl7222tandon <cl7222@nyu.edu>
2025-02-26 01:19:46 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_priority_encoder (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,10 +16,33 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    reg [7:0] C;
+    wire [15:0] In;
+
+    assign In = {uio_in, ui_in};
+
+    always @(*) begin
+        C = 8'b11110000;  
+        if (In[15]) C = 8'd15;
+        else if (In[14]) C = 8'd14;
+        else if (In[13]) C = 8'd13;
+        else if (In[12]) C = 8'd12;
+        else if (In[11]) C = 8'd11;
+        else if (In[10]) C = 8'd10;
+        else if (In[9])  C = 8'd9;
+        else if (In[8])  C = 8'd8;
+        else if (In[7])  C = 8'd7;
+        else if (In[6])  C = 8'd6;
+        else if (In[5])  C = 8'd5;
+        else if (In[4])  C = 8'd4;
+        else if (In[3])  C = 8'd3;
+        else if (In[2])  C = 8'd2;
+        else if (In[1])  C = 8'd1;
+        else if (In[0])  C = 8'd0;
+    end
+
+    assign uo_out = C;
+
 
   // List all unused inputs to prevent warnings
   wire _unused = &{ena, clk, rst_n, 1'b0};
